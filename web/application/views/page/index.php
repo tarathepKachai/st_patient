7628f54eb50c9ef86ef74ee6f2a3b8d0877a6033
tarathepKachai <tarathep7904@gmail.com>
2018-06-04 16:35:22 +0700

    [web] save patient (in progress 2)

--- web/application/views/page/index.php
+++ web/application/views/page/index.php
@@ -29,33 +29,91 @@
             </tr>
 
             <tr>
-                <td class="menu_choice" id="choice_1">
+                <td class="menu_choice enable_choice" id="choice_1">
                     บันทึกผู้ป่วย
                 </td>
                 <td style="padding: 10px;vertical-align: top" rowspan="4">
                     <div style="display: block;"  id="choice1">
                         <form name="user_save" id="user_save" action="#" method="POST">
-                            <label class="label_1">วัน/เดือน/ปี </label> &nbsp; <input type="text" name="rec_day" id="datepicker2" class="rec_day" > <br>
+                            <label class="label_1">วัน/เดือน/ปี </label> &nbsp; <input  type="text" name="rec_day" id="datepicker2" class="rec_day" > 
+                            <span class="error_report" id="rec_day_error" ></span>
+                            <br>
+
                             <label class="label_1" >คำนำหน้านาม </label>  
                             &nbsp;<label><input type="radio" class="radio_but" name="prefix" value="1" > ด.ญ. &nbsp; </label>
                             &nbsp;<label><input type="radio" class="radio_but" name="prefix" value="2" > ด.ช. &nbsp; </label>
                             &nbsp;<label><input type="radio" class="radio_but" name="prefix" value="3" > นาย &nbsp; </label>
                             &nbsp;<label><input type="radio" class="radio_but" name="prefix" value="4" > นาง &nbsp; </label>
                             &nbsp;<label><input type="radio" class="radio_but" name="prefix" value="5" > นางสาว &nbsp; </label>
-                            &nbsp;<label class="label_1" >ชื่อ</label> <input type="text" class="name_medium" name="name" id="name"> 
+                            &nbsp;<span class="error_report" id="prefix_error" ></span>
+                            <label class="label_1" >ชื่อ</label> <input type="text" class="name_medium" name="name" id="name"> 
+                            <span class="error_report" id="name_error" ></span>
                             &nbsp;<label  class="label_1">นามสกุล</label> <input type="text" class="name_medium" name="lastname" id="lastname"> 
+                            <span class="error_report" id="lastname_error" ></span>
                             <br>
+
                             <label class="label_1" >เพศ </label>  
-                            &nbsp;<label><input type="radio" class="radio_but" name="gender" value="1" > ชาย &nbsp; </label>
-                            &nbsp;<label><input type="radio" class="radio_but" name="gender" value="2" > หญิง &nbsp; </label>
+                            &nbsp;<label><input type="radio" class="radio_but" name="gender"  value="1" > ชาย &nbsp; </label>
+                            &nbsp;<label><input type="radio" class="radio_but" name="gender"  value="2" > หญิง &nbsp; </label>
+                            &nbsp;<span class="error_report" id="gender_error" ></span>
                             <label class="label_1">วัน/เดือน/ปีเกิด </label> &nbsp; <input type="text" name="birthday" id="birthday" class="rec_day" >
-                            &nbsp;<label style="margin-left: 40px" class="label_1">เลขที่บัตรประชาชน</label> <input type="text" class="name_medium" name="id_card" id="id_card"> 
+                            &nbsp;<span class="error_report" id="birthday_error" ></span>
+                            <label  class="label_1">เลขที่บัตรประชาชน</label> <input type="text" class="name_medium" maxlength="13" name="id_card" id="id_card">
+                            &nbsp;<span class="error_report" id="id_card_error" ></span>
                             <br>
-                            <label class="label_1" >น้ำหนัก </label>  <input type="text" class="text_short" name="weight" id="weight"> 
-                            &nbsp; <label class="label_1" >ส่วนสูง </label>  <input type="text" class="text_short" name="height" id="height"> 
+
+                            <label class="label_1" >น้ำหนัก </label>  
+                            <input type="text" class="text_short" name="weight" id="weight">
+                            <span class="error_report" id="w_error" ></span> 
+                            &nbsp; <label class="label_1" >ส่วนสูง </label>  
+                            <input type="text" class="text_short" name="height" id="height"> 
+                            <span class="error_report" id="h_error" ></span> 
+                            &nbsp; <label class="label_1" >ตำหนิ/แผลเป็น </label>  
+                            <input type="text" class="rec_day" style="width: 300px" name="height" id="height"> 
+                            <span class="error_report" id="h_error" ></span> 
+                            <br>
+                            <label class="label_1">สถานภาพ</label> 
+                            &nbsp;<label><input type="radio" class="radio_but" name="status" value="1" > โสด &nbsp; </label>
+                            &nbsp;<label><input type="radio" class="radio_but" name="status" value="2" > สมรส &nbsp; </label>
+                            &nbsp;<label><input type="radio" class="radio_but" name="status" value="3" > หย่าร้าง &nbsp; </label>
+                            &nbsp;<label><input type="radio" class="radio_but" name="status" value="4" > มีบุตร &nbsp; </label>
+                            &nbsp; <label class="label_1" >อาชีพ</label> <input type="text" name="job" id="job" class="name_medium" > 
+                            <hr>
+                            <label class="label_1">ที่อยู่หรือสถานที่ติดต่อได้</label>
+                            &nbsp;บ้านเลขที่ <input type="text" class="text_short" name="h_no" id="h_no"> 
+                            &nbsp; ถนน <input type="text" class="text_short" name="road_no" id="road_no">
+                            &nbsp; ตำบล/แขวง <input type="text" class="text_short" name="tumbon" id="tumbon">
+                            <br>
+
+                            อำเภอ <input type="text" class="text_short" name="aumpur" id="aumpur"> 
+                            &nbsp; จังหวัด <input type="text" class="text_short" name="province" id="province">
+                            &nbsp; โทรศัพท์ที่สามารถติดต่อได้ <input type="text" class="name_medium" name="tel" id="tel">
+                            <br>
+
+                            โทรสาร <input style="margin-top: 7px" type="text" class="name_medium" name="fax" id="fax"> 
+                            &nbsp; Line ID <input type="text" class="name_medium" name="line" id="line">
+                            &nbsp; E-mail <input type="email" class="name_medium" name="email" id="email">
+                            <hr>
+
+                            <label class="label_1" >วุฒิการศึการสูงสุด</label> &nbsp; <input type="text" class="name_medium" style="width: 400px;margin-top:7px;">
+                            <br>
+
+                            <label class="label_1">ประสบการณ์ด้านการเรียนการสอน ทีเกี่ยวข้องกับนักศึกษาแพทย์ แพทย์ใช้ทุน/แพทย์ประจำบ้าน ฯลฯ</label>
+                            <br>
+                            <label class="label_1"><input type="radio" class="radio_but" style="margin-left:20px;" name="exp" value="1" /> เคย </label> 
+                            <label ><input type="checkbox" class="radio_but" style="margin-left:10px;" name="exp_1" id="exp_1" /> ซักประวัติอาการ </label>  <input type="text" class="name_medium" style="width: 250px" name="exp_1_detail" id="exp_1_detail">
+                            <label ><input type="checkbox" class="radio_but" style="margin-left:10px;" name="exp_2" id="exp_1" /> ให้คำแนะนำอาการ/โรค </label>  <input type="text" class="name_medium" style="width: 250px" name="exp_2_detail" id="exp_1_detail">
+                            <br>
+                            <label ><input type="checkbox" class="radio_but" style="margin-left:78px;" name="exp_3" id="exp_1" /> ตรวจร่างกายอาการ </label>  <input type="text" class="name_medium" style="width: 250px" name="exp_3_detail" id="exp_1_detail">
+                            <label ><input type="checkbox" class="radio_but" style="margin-left:10px;" name="exp_4" id="exp_1" /> การทำหัตถการ </label>  <input type="text" class="name_medium" style="width: 250px" name="exp_4_detail" id="exp_1_detail">
+                            <br>
+                            <label class="label_1"><input type="radio" class="radio_but" style="margin-left:20px;" name="exp" value="2" /> ไม่เคย </label> 
+                            <hr>
+                            
+                            <label class="label_1">ประวัติการเจ็บป่วยเข้ารับการรักษาในโรงพยาบาลการผ่าตัดหรือได้รับบริการทางการแพทย์อย่างไรบ้าง</label>
                             
                         </form>
-
+                        <button onclick="submit()">Submit</button>
 
                     </div>
                     <div style="display: none;" id="choice2">
--- web/assets/my_js/index_page.js
+++ web/assets/my_js/index_page.js
@@ -1,53 +1,161 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-    
-    $("#choice_1").click(function(){
-       $("#choice_1").removeClass("disable_choice");
-         $("#choice_1").addClass("enable_choice");
-        $("#choice1").css({"display":"block"});
-        
+
+
+    $("input[name='gender']").click(function () {
+        $("#gender_error").html("");
+    });
+
+    $("input[name='prefix']").click(function () {
+        $("#prefix_error").html("");
+    });
+
+    $('#name, #lastname,#weight,#height,#id_card,#birthday,#datepicker2').focusout(function () {
+        if (this.value !== null) {
+            var id = this.id;
+            $('#' + id).css({"box-shadow":"0px 0px 0px black","border-color":"black"});
+        }
+    });
+
+    $("#choice_1").click(function () {
+        $("#choice_1").removeClass("disable_choice");
+        $("#choice_1").addClass("enable_choice");
+        $("#choice1").css({"display": "block"});
+
         $("#choice_2").removeClass("enable_choice");
         $("#choice_2").addClass("disable_choice");
-        $("#choice2").css({"display":"none","background-color":"white"});
-        
-        
+        $("#choice2").css({"display": "none", "background-color": "white"});
+
+
         $("#choice_3").removeClass("enable_choice");
-         $("#choice_3").addClass("disable_choice");
-         $("#choice3").css({"display":"none","background-color":"white"});
+        $("#choice_3").addClass("disable_choice");
+        $("#choice3").css({"display": "none", "background-color": "white"});
     });
-    
-    $("#choice_2").click(function(){
-         $("#choice_1").removeClass("enable_choice");
-         $("#choice_1").addClass("disable_choice");
-        $("#choice1").css({"display":"none","background-color":"white"});
-        
+
+    $("#choice_2").click(function () {
+        $("#choice_1").removeClass("enable_choice");
+        $("#choice_1").addClass("disable_choice");
+        $("#choice1").css({"display": "none", "background-color": "white"});
+
         $("#choice_2").removeClass("disable_choice");
         $("#choice_2").addClass("enable_choice");
-        $("#choice2").css("display","block");
-        
-        
+        $("#choice2").css("display", "block");
+
+
         $("#choice_3").removeClass("enable_choice");
-         $("#choice_3").addClass("disable_choice");
-         $("#choice3").css({"display":"none","background-color":"white"});
+        $("#choice_3").addClass("disable_choice");
+        $("#choice3").css({"display": "none", "background-color": "white"});
     });
-    
-    $("#choice_3").click(function(){
-       $("#choice_1").removeClass("enable_choice");
-         $("#choice_1").addClass("disable_choice");
-        $("#choice1").css({"display":"none","background-color":"white"});
-        
+
+    $("#choice_3").click(function () {
+        $("#choice_1").removeClass("enable_choice");
+        $("#choice_1").addClass("disable_choice");
+        $("#choice1").css({"display": "none", "background-color": "white"});
+
         $("#choice_2").removeClass("enable_choice");
         $("#choice_2").addClass("disable_choice");
-        $("#choice2").css({"display":"none","background-color":"white"});
-        
-        
+        $("#choice2").css({"display": "none", "background-color": "white"});
+
+
         $("#choice_3").removeClass("disable_choice");
-         $("#choice_3").addClass("enable_choice");
-         $("#choice3").css("display","block");
+        $("#choice_3").addClass("enable_choice");
+        $("#choice3").css("display", "block");
     });
 });
 
+function submit() {
 
+    if ($("#datepicker2").val() === "") {
+        $("#datepicker2").attr("placeholder", "กรอกวันที่");
+        $("#datepicker2").css({"box-shadow":"0px 0px 10px red","border-color":"red"});
+        //$("#rec_day_error").html("กรอกวันที่");
+    } else {
+        //$("#rec_day_error").html("");
+        $("#datepicker2").attr("placeholder", "");
+        $("#datepicker2").css({"box-shadow":"0px 0px 0px black","border-color":"black"});
+    }
+
+    if (!$("input[name='prefix']").is(':checked')) {
+        $("#prefix_error").html("เลือกคำนำหน้านาม");
+    } else {
+        $("#prefix_error").html("");
+    }
+
+    if ($("#name").val() === "") {
+        //$("#name_error").html("กรอกชื่อ");
+        $("#name").attr("placeholder", "กรอกชื่อ");
+        $("#name").css({"box-shadow":"0px 0px 10px red","border-color":"red"});
+    } else {
+        //$("#name_error").html("");
+        $("#name").attr("placeholder", "");
+        $("#name").css({"box-shadow":"0px 0px 0px black","border-color":"black"});
+    }
+
+    if ($("#lastname").val() === "") {
+        // $("#lastname_error").html("กรอกนามสกุล");
+        $("#lastname").attr("placeholder", "กรอกนามสกุล");
+        $("#lastname").css({"box-shadow":"0px 0px 10px red","border-color":"red"});
+        
+    } else {
+        //$("#lastname_error").html("");
+        $("#lastname").attr("placeholder", "");
+        $("#lastname").css({"box-shadow":"0px 0px 0px black","border-color":"black"});
+        
+       
+    }
+
+    if (!$("input[name='gender']").is(':checked')) {
+        $("#gender_error").html("เลือกเพศ");
+    } else {
+        $("#gender_error").html("");
+    }
+
+
+    if ($("#birthday").val() === "") {
+        //$("#birthday_error").html("กรอกวันที่");
+        $("#birthday").attr("placeholder", "กรอกวันที่");
+        $("#birthday").css({"box-shadow":"0px 0px 10px red","border-color":"red"});
+    } else {
+        // $("#birthday_error").html("");
+        $("#birthday").attr("placeholder", "");
+        $("#birthday").css({"box-shadow":"0px 0px 0px black","border-color":"black"});
+    }
+
+    if ($("#id_card").val() === "") {
+        // $("#id_card_error").html("กรอกเลขบัตรประชาชน");
+        $("#id_card").attr("placeholder", "กรอกเลขบัตรประชาชน");
+        $("#id_card").css({"box-shadow":"0px 0px 10px red","border-color":"red"});
+    } else if ($("#id_card").val().length!== 13) {
+        $("#id_card_error").html("กรอกเลขบัตรประชาชน13หลัก");
+
+        $("#id_card").css({"box-shadow":"0px 0px 10px red","border-color":"red"});
+    } else {
+        $("#id_card_error").html("");
+        $("#id_card").attr("placeholder", "");
+        $("#id_card").css({"box-shadow":"0px 0px 0px black","border-color":"black"});
+    }
+    
+    
+    if ($("#weight").val() === "") {
+        //$("#w_error").html("กรอกน้ำหนัก");
+        $("#weight").attr("placeholder", "กรอกน้ำหนัก");
+        $("#weight").css({"box-shadow":"0px 0px 10px red","border-color":"red"});
+    } else {
+        //$("#w_error").html("");
+        $("#weight").attr("placeholder", "");
+        $("#weight").css({"box-shadow":"0px 0px 0px black","border-color":"black"});
+    }
+    if ($("#height").val() === "") {
+        // $("#h_error").html("กรอกส่วนสูง");
+        $("#height").attr("placeholder", "กรอกส่วนสูง");
+        $("#height").css({"box-shadow":"0px 0px 10px red","border-color":"red"});
+    } else {
+        // $("#h_error").html("");
+        $("#height").attr("placeholder", "");
+        $("#height").css({"box-shadow":"0px 0px 0px black","border-color":"black"});
+    }
+
+}
 
 
 
